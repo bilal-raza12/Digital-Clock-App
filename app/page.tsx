b68327f8edbac5b361ffe0cfb4b462c62ac2e215
bilal-raza12 <muhammadbilalraza497@gmail.com>
2024-09-11 21:12:18 +0500

    My App

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import DigitalClockComponent from "@/components/ui/digital-clock";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+    <DigitalClockComponent />
+      
+    </div>
+  );
+}
